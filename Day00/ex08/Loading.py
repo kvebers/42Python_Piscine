c925a2e1a74952a36ba9f141db4de62a95d788fe
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-28 23:38:51 +0200

    Ex08

---
+++ Day00/ex08/Loading.py
@@ -0,0 +1,16 @@
+"""
+Takes the elements itterates trought the list of elements
+numbers is static in C, 
+yield return value that is continiusly modified
+"""
+
+
+def ft_tqdm(lst: range) -> None:
+    total = len(lst)
+    for number, elem in enumerate(lst, 1):
+        progress = int(number / total * 100)
+        bar = "▓" * progress + " " + " " * (100 - progress)
+        if progress == 100:
+            bar = "▓" * progress
+        print(f"{progress}%|{bar}| {number}/{total} + \r")
+        yield elem
