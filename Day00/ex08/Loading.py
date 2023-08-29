b1fc4450b1bff7a845e624014f92f77042864a75
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-29 09:32:24 +0200

    Fixed some issues

--- Day00/ex08/Loading.py
+++ Day00/ex08/Loading.py
@@ -1,3 +1,7 @@
+import sys
+
+
+
 """
 Takes the elements itterates trought the list of elements
 numbers is static in C, 
@@ -9,8 +13,8 @@ def ft_tqdm(lst: range) -> None:
     total = len(lst)
     for number, elem in enumerate(lst, 1):
         progress = int(number / total * 100)
-        bar = "▓" * progress + " " + " " * (100 - progress)
+        bar = "▓" * progress + "|" + " " * (100 - progress)
         if progress == 100:
             bar = "▓" * progress
-        print(f"{progress}% | {bar} | {number}/{total}  \r")
+        print(f"{progress}% |{bar}| {number}/{total}  \r", end = '', flush=True, file=sys.stderr)
         yield elem
--- Day00/ex08/tester.py
+++ Day00/ex08/tester.py
@@ -6,7 +6,5 @@ from Loading import ft_tqdm
 if __name__ == "__main__":
     for elem in ft_tqdm(range(333)):
         sleep(0.005)
-        print()
     for elem in tqdm(range(333)):
         sleep(0.005)
-        print()
