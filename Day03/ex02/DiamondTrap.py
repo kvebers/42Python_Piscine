b3eeca1212d3adb8e65a85d97e6dc9ad827d1c9f
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-30 15:07:24 +0200

    Dimond trap solution for Python

--- Day03/ex02/DiamondTrap.py
+++ Day03/ex02/DiamondTrap.py
@@ -2,3 +2,22 @@ from S1E7 import Baratheon, Lannister
 
 
 class King(Baratheon, Lannister):
+    def __init__(self, first_name, is_alive=True):
+        """Specificly forcing it to inherit from Baratheon"""
+        Baratheon.__init__(self, first_name, is_alive)
+    
+    def set_eyes(self, color):
+        """setter for eye color"""
+        self.eyes = color
+
+    def set_hairs(self, color):
+        """setter for hair color"""
+        self.hairs = color
+
+    def get_eyes(self):
+        """getter for eye color"""
+        return self.eyes
+
+    def get_hairs(self):
+        """getter for hair"""
+        return self.hairs
