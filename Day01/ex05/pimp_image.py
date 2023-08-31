17e6abd2b79c1271fee0a2d5e54b6d409e117f86
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-08-31 10:54:36 +0200

    Fix of the grayscale

--- Day01/ex05/pimp_image.py
+++ Day01/ex05/pimp_image.py
@@ -10,21 +10,6 @@ import matplotlib.pyplot as plt
 Substractin from 255 the original value
 """
 
-def ft_grey_smooth(array) ->np.ndarray:
-    try:
-        newArray = array.copy()
-        newArray[:, :, 0] = newArray[:, :, 0] * 0.2989
-        newArray[:, :, 1] = newArray[:, :, 1] * 0.5870
-        newArray[:, :, 2] = newArray[:, :, 2] * 0.1140
-        return ft_grey(newArray)
-    except FileNotFoundError:
-        print(f"Error: File '{path}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    except AssertionError as error:
-        print(AssertionError.__name__ + ":" + str(error))
-    return []
-
 def ft_invert(array) -> np.ndarray:
     try:
         newArray = array.copy()
@@ -102,10 +87,12 @@ Leaving Open only Blue channel
 def ft_grey(array) -> np.ndarray:
     try:
         newArray = array.copy()
-        # newArray[:, :, 0] = (array[0:, :, 0] + array[0:, :, 1] + array[0:, :, 2]) / 3
-        # newArray[:, :, 1] = (array[0:, :, 0] + array[0:, :, 1] + array[0:, :, 2]) / 3 
-        # newArray[:, :, 2] = (array[0:, :, 0] + array[0:, :, 1] + array[0:, :, 2]) / 3 
-        np.mean(newArray, axis=2)
+        newArray[:, :, 0] = newArray[:, :, 0] * 0.2989
+        newArray[:, :, 1] = newArray[:, :, 1] * 0.5870
+        newArray[:, :, 2] = newArray[:, :, 2] * 0.1140
+        newArray[:, :, 0] = (newArray[0:, :, 0] + newArray[0:, :, 1] + newArray[0:, :, 2]) / 3
+        newArray[:, :, 1] = (newArray[0:, :, 0] + newArray[0:, :, 1] + newArray[0:, :, 2]) / 3 
+        newArray[:, :, 2] = (newArray[0:, :, 0] + newArray[0:, :, 1] + newArray[0:, :, 2]) / 3 
         return (newArray)
     except FileNotFoundError:
         print(f"Error: File '{path}' not found.")
--- Day01/ex05/tester.py
+++ Day01/ex05/tester.py
@@ -1,6 +1,6 @@
 from load_image import ft_load
 from pimp_image import ft_blue, ft_red, ft_green
-from pimp_image import ft_invert, ft_grey, ft_grey_smooth
+from pimp_image import ft_invert, ft_grey
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
@@ -11,22 +11,21 @@ def main():
         greenArray = ft_green(image)
         redArray = ft_red(image)
         greyArray = ft_grey(image)
-        greySmoothArray = ft_grey_smooth(image)
         invertedArray = ft_invert(image)
+
         GreenImage = Image.fromarray(greenArray)
         RedImage = Image.fromarray(redArray)
         GreyImage = Image.fromarray(greyArray)
         BlueImage = Image.fromarray(blueArray)
         InvertedImage = Image.fromarray(invertedArray)
-        GreySmooth = Image.fromarray(greySmoothArray)
         OriginalImage = Image.fromarray(image)
+
         GreenImage.show()
         RedImage.show()
         GreyImage.show()
         BlueImage.show()
         OriginalImage.show()
         InvertedImage.show()
-        GreySmooth.show()
     except FileNotFoundError:
         print(f"Error: File '{path}' not found.")
     except AssertionError as error:
