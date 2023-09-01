63a1929f0f423d8ba9fa4202eaa6cbdd9af8eab8
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-01 15:38:18 +0200

    ex01 inner global variables

--- Day04/ex01/in_out.py
+++ Day04/ex01/in_out.py
@@ -1,10 +1,16 @@
 def square(x: int | float) -> int | float:
-    return (x ** 2)
+    return x ** 2
+
 
 def pow(x: int | float) -> int | float:
+    return x ** x
 
 
 def outer(x: int | float, function) -> object:
 
+    def inner() -> float:
+        nonlocal x
+        x = function(x)
+        return x
 
-def inner() -> float:
+    return inner
