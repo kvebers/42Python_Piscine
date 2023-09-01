43564e8abe674324545f1e7a494a35dcbc497b07
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-01 19:12:47 +0200

    Remade Excersise 00 and 01

--- Day02/ex01/aff_life.py
+++ Day02/ex01/aff_life.py
@@ -1,37 +1,38 @@
 from load_csv import load
-from ft_filter import ft_filter
 import matplotlib.pyplot as plt
 
 
-def is_Germany(row):
-    return row[0] == "Germany"
+def isCountry(row, countrColumn, targerCountry):
+    """Specify the Country here"""
+    return row[countrColumn] == targerCountry
+
 
 def main():
+    """Displays the information about the countries"""
     try:
-        """
-        Preping the data set and selecting Germany and Years
-        """
         data = load("life_expectancy_years.csv")
-        Germany = ft_filter(is_Germany, data)
-        Germany = Germany[0][1:]
-        Years = data[0]
-        Years = Years[1:]
-        """
-        Just ploting the values against the data
-        """
-        plt.plot(Years, Germany,'r')
+        # France = data[data.apply(isCountry, axis=1, args=(0, "France"))]
+        # print(France)
+        # France = France.iloc[:, 1:]
+        Germany = data[data.apply(isCountry, axis=1, args=(0, "Germany"))]
+        # print(Germany)
+        Germany = Germany.iloc[:, 1:]
+        Latvia = data[data.apply(isCountry, axis=1, args=(0, "Latvia"))]
+        # print(Latvia)
+        Latvia = Latvia.iloc[:, 1:]
+        Years = data.columns[1:]
+        plt.plot(Years, Germany.iloc[0], 'r', label="Germany")
+        plt.plot(Years, Latvia.iloc[0], 'b', label="Latvia")
+        # plt.plot(Years, France.iloc[0], 'g', label="France")
         plt.xlabel("Years")
         plt.ylabel("Life Expectancy")
         plt.xticks(Years[::50])
-        plt.yticks(Germany[::50])
-        plt.title("Life Expectancy in Germany Over the Years")
+        plt.yticks(range(30, 101, 10))
+        plt.title("Life Expectancy Over the Years")
+        plt.legend()
         plt.show()
-    except FileNotFoundError:
-        print(f"Error: File '{path}' not found.")
     except Exception as e:
         print(f"An error occurred: {e}")
-    except AssertionError as e:
-        print(AssertionError.__name__ + ":" + str(e))
 
 
 if __name__ == "__main__":
