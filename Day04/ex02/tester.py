9579ef642a447dcf98d0f2c209e4916753f106d4
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-09-01 15:51:48 +0200

    Started ex02 Day04

---
+++ Day04/ex02/tester.py
@@ -0,0 +1,19 @@
+from callLimit import callLimit
+
+
+def main():
+    @callLimit(3)
+    def f():
+        print("f()")
+
+    @callLimit(1)
+    def g():
+        print("g()")
+
+    for i in range(3):
+        f()
+        g()
+
+
+if __name__ == "__main__":
+    exit(main())
